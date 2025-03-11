f8d52b1a37e009ecad52d7e0bf7973d49ce37f18
hosseinkhashaypour <h20062006p@gmail.com>
2025-03-12 02:18:59 +0330

    add nav bar and some logic

---
+++ lib/const/theme/colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color(0xFFFFD700);
+const primary2Color = Color(0xFF292929);
+const primary3Color = Color(0xFF0026EB);
+const linkColor = Color(0xFF0094FF);
+const buttonColor = Color(0xFFFFB800);
+const boxColors = Color(0xFFADADAD);
+const darkColor = Color(0xFF181818);
+const textFieldColor = Color(0xFFF1F2F4);
+const backGroundColor = Color(0xFFFFFFFF);
+const buttonBgColor = Color(0xFF00E115);
+// const backGroundColor = Color(0xFFF5F5F5);
+
+const iconColor = Color(0xffffa600);
+const whiteColor = Colors.white;
+final greyColor = Colors.grey.shade300;
